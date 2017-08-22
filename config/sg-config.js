4e29b498b3ae58737e5cd71e8c3bf987174ca428
Joshmurph316 <Joshmurph316@gmail.com>
2017-08-22 17:14:37 -0400

    min commit

---
+++ config/sg-config.js
@@ -0,0 +1,7 @@
+var sgKey = {
+    key: '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',
+    fromEmail: 'josh-murphy@noReply',
+    toEmail: 'Joshmurphy@webdevjosh.com'
+};
+
+module.exports = sgKey;
